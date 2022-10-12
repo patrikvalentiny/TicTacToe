ab14dbdb0f0cfdcf136f93a323d6c019ba30da29
Matej Mazur <matejmazur@outlook.com>
2022-10-12 11:00:01 +0200

    RevertLastCommit

--- TicTacToe/src/tictactoe/gui/TicTacToe.java
+++ TicTacToe/src/tictactoe/gui/TicTacToe.java
@@ -7,13 +7,9 @@ package tictactoe.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Control;
 import javafx.scene.image.Image;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 /**
@@ -26,14 +22,13 @@ public class TicTacToe extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        /*Parent root = FXMLLoader.load(getClass().getResource("views/MenuScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/MenuScreen.fxml"));
         stage.setTitle("Tic Tac Toe");
         stage.getIcons().add(new Image(getClass().getResource("images/X.png").toExternalForm()));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.centerOnScreen();
-        stage.setResizable(false);*/
-        generateBoard(10, stage);
+        stage.setResizable(false);
         stage.show();
 
     }
@@ -44,33 +39,6 @@ public class TicTacToe extends Application
     public static void main(String[] args)
     {
         launch(args);
-    }
-
-    public void generateBoard(int number, Stage _stage)
-    {
-        Scene scene = new Scene(new Group());
-        GridPane grid = new GridPane();
-        for (int i = 0; i < number; i++) {
-            ColumnConstraints colC = new ColumnConstraints();
-                colC.setMinWidth(30);
-                colC.setMaxWidth(100);
-                colC.setHgrow(Priority.ALWAYS);
-            grid.getColumnConstraints().add(colC);
-            RowConstraints rowC = new RowConstraints();
-                rowC.setPrefHeight(100);
-                rowC.setVgrow(Priority.ALWAYS);
-            grid.getRowConstraints().add(rowC);
-        }
-        for (int i = 0; i < number; i++) {
-            for (int j = 0; j < number; j++) {
-                var btn = new Button("");
-                btn.maxWidth(Control.USE_PREF_SIZE);
-                btn.maxHeight(Integer.MAX_VALUE);
-                grid.add(btn, i, j);
-            }
-        }
-        scene.setRoot(grid);
-        _stage.setScene(scene);
     }
     
 }
